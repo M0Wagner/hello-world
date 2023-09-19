a35b2b667afdb96fd1c35e314543e64ef402a192
M0Wagner <moritz.wagner06@gmx.at>
2023-09-19 09:05:44 +0200

    weojuihgfsoie

---
+++ helloworld/src/main/java/htlleonding/ac/at/App.java
@@ -0,0 +1,13 @@
+package htlleonding.ac.at;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
